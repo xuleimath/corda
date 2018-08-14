190ad8e434507b0a3877fe4b4807b4b71d5267fb
Dominic Fox <dominic.fox@r3.com>
2018-08-14 11:23:04 +0100

    constructorForDeserialization no longer returns null

--- serialization/src/main/kotlin/net/corda/serialization/internal/amqp/SerializationOutput.kt
+++ serialization/src/main/kotlin/net/corda/serialization/internal/amqp/SerializationOutput.kt
@@ -51,6 +51,7 @@ open class SerializationOutput @JvmOverloads constructor(
             return _serialize(obj, context)
         } catch (amqp: AMQPNotSerializableException) {
             amqp.log("Serialize", logger)
+            amqp.printStackTrace()
             throw NotSerializableException(amqp.mitigation)
         } finally {
             andFinally()
